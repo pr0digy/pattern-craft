9dcaf892302fc811fd88286b632185059b04841f
Alex Golovanov <alex.golovanov@gmail.com>
2020-08-21 12:45:59 +0300

    Fixed SSR for JSS

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+	root: {
+		position: 'fixed',
+		right: 50,
+		bottom: '50px',
+	},
+});
 
 export default function App() {
+	const styles = useStyles();
 	return (
 		<div className="App">
 			<div>Hi!</div>
+			<Fab color="primary" aria-label="add" className={styles.root}>
+				<KeyboardArrowUpIcon />
+			</Fab>
 		</div>
 	);
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
 import 'normalize.css';
 
 export default function MyApp({ Component, pageProps }) {
+	React.useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector('#jss-server-side');
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles);
+		}
+	}, []);
+
 	return <Component {...pageProps} />;
 }
